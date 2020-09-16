6713e76c6fc2f32899cae22b24643ffdbb36d462
Sonali Shukla <sonalikatara@gmail.com>
2020-09-16 04:58:12 -0700

    feat: add page query to movies API

--- routes/movies.js
+++ routes/movies.js
@@ -5,13 +5,15 @@ var router = express.Router();
 
 
 const API_URL_POPULAR_MOVIES =
-  "https://api.themoviedb.org/3/movie/popular?api_key="+process.env.TMBC_API_KEY+"&language=en-US&page=1";
-
+  "https://api.themoviedb.org/3/movie/popular?api_key="+process.env.TMBC_API_KEY+"&language=en-US";
+  
 /* GET moviess listing. */
 router.get('/', function(req, res, next) {
-axios.get(API_URL_POPULAR_MOVIES).then(response => res.send(response.data.results)).catch(err => {
-  res.send(err);
-});
+  let page =  req.query.page ? req.query.page : 1;
+  let reqUrl = API_URL_POPULAR_MOVIES + '&page='+ page;
+  axios.get(reqUrl).then(response => res.send(response.data.results)).catch(err => {
+    res.send(err);
+  });
   
 });
 
