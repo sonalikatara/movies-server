2b90cd033f96cba64f1511cbce0cff0b4b114a07
Sonali Shukla <sonalikatara@gmail.com>
2020-09-16 14:05:03 -0700

    add ratings to movie data

--- routes/movies.js
+++ routes/movies.js
@@ -2,17 +2,15 @@ var axios = require('axios');
 var express = require('express');
 var router = express.Router();
 
-
-
 const API_URL_POPULAR_MOVIES =
   "https://api.themoviedb.org/3/movie/popular?api_key="+process.env.TMBC_API_KEY+"&language=en-US";
   
-/* GET moviess listing. */
+/* GET movies listing. */
 router.get('/', function(req, res, next) {
   let page =  req.query.page ? req.query.page : 1;
   let reqUrl = API_URL_POPULAR_MOVIES + '&page='+ page;
   axios.get(reqUrl).then(response => res.send(response.data.results)).catch(err => {
-    res.send(err);
+    res.send("error while getting popular movies results "+ err);
   });
   
 });
---
+++ routes/search.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+
+const API_URL_SEARCH_MOVIES = "https://api.themoviedb.org/3/search/movie?api_key="+process.env.TMBC_API_KEY+"&language=en-US";
+/* test API. */
+router.get('/',function(req,res,next){
+  let query =  req.query.query ? req.query.query : "popular";
+  let page =  req.query.page ? req.query.page : 1;
+  let reqUrl = API_URL_SEARCH_MOVIES + '&page='+ page+'&query='+query;
+  axios.get(reqUrl).then(response => res.send(response.data.results.map(movie => ({...movie, rating : Math.round(movie.vote_average/2 )})))).catch(err => {
+    res.send("error while getting search results " + err);
+  });
+});
+
+module.exports = router;
