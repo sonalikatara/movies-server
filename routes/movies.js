3e591fe1fa502b40c6aee40cc6b3c5d3c1456f1f
Sonali Shukla <sonalikatara@gmail.com>
2020-09-16 21:58:43 -0700

    cleanup

--- routes/movies.js
+++ routes/movies.js
@@ -5,14 +5,14 @@ var router = express.Router();
 const API_URL_POPULAR_MOVIES =
   "https://api.themoviedb.org/3/movie/popular?api_key="+process.env.TMBC_API_KEY+"&language=en-US";
   
-/* GET movies listing. */
+/* GET popular movies listing. */
 router.get('/', function(req, res, next) {
   let page =  req.query.page ? req.query.page : 1;
   let reqUrl = API_URL_POPULAR_MOVIES + '&page='+ page;
   axios.get(reqUrl).then(response => res.send(response.data.results)).catch(err => {
     res.send("error while getting popular movies results "+ err);
   });
-  
-});
+}
+);
 
 module.exports = router;
