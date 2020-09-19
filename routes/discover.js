7424118fa21d93d43dfcfd293f2f544208d3b969
Sonali Shukla <sonalikatara@gmail.com>
2020-09-18 18:14:59 -0700

    debug: discover api giving incorrect results

--- routes/discover.js
+++ routes/discover.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
-const API_URL_DISCOVER_MOVIES = "https://api.themoviedb.org/3/discover/movie?api_key="+process.env.TMBC_API_KEY+"&language=en-US&include_adult=false&include_video=false&sort_by=popularity.desc";
+const API_URL_DISCOVER_MOVIES = "https://api.themoviedb.org/3/discover/movie?api_key="+process.env.TMBC_API_KEY+"&language=en-US&include_adult=false&include_video=false&sort_by=popularity.desc&page=1";
 
 /*  GET popular movies listing based on a QUERY. */
 router.get('/',function(req,res,next){
@@ -10,6 +10,8 @@ router.get('/',function(req,res,next){
   let queryValue = req.query.queryValue ? req.query.queryValue : "popularity.desc";
   let page =  req.query.page ? req.query.page : 1;
   let reqUrl = `${API_URL_DISCOVER_MOVIES}&page=${page}&${query}=${queryValue}`;
+  console.log("####",reqUrl);
+
   axios.get(reqUrl).then(
     response => res.send(
       response.data.results.map(
